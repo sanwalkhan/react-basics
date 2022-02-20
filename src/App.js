38ac54af2f5c5a0ded3b1a1139a509f629ee20ce
Sanwal Khan <71782878+sanwalkhan@users.noreply.github.com>
2022-02-20 14:01:56 +0500

    changes
    changes

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import './App.css';
+import { Cards } from './Components/cards';
 
 function App() {
   return (
     <div className="App">
-      <h1>Sanwal Khan</h1>
+    <h1>Top Shoe Brand In World</h1>
+      <Cards/>
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,28 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-// import App from './App';
+import App from './App';
 import reportWebVitals from './reportWebVitals';
 
 
-let currdate = new Date()
-currdate = currdate.getHours()  
-let greeting = '';
-
-if(currdate >= 1 && currdate< 12){
-  greeting = 'Good Morning'
-}else if(currdate >=12 && currdate <20){
-  greeting = "Good Evening"
-}else{
-  greeting = 'Good Night'
-}
 
 ReactDOM.render(
- <React.Fragment>
- <h1>Hello Sir, {greeting} </h1>
-
-
- </React.Fragment>,
+ <App/>,
   document.getElementById('root')
 );
 
