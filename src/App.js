d70a05543d0ebf143ead847ed34d40a1df4d8cc6
Sanwal Khan <71782878+sanwalkhan@users.noreply.github.com>
2022-02-23 12:51:02 +0500

    hooks

--- src/App.js
+++ src/App.js
@@ -1,11 +1,9 @@
 import './App.css';
-import { Cards } from './Components/cards';
 
 function App() {
   return (
     <div className="App">
-    <h1>Top Shoe Brand In World</h1>
-      <Cards/>
+    Sanwal kHAN
     </div>
   );
 }
