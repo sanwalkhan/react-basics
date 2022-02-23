093f51d333db4a8a9c6eec06fa2bf2898984246d
Sanwal Khan <71782878+sanwalkhan@users.noreply.github.com>
2022-02-23 14:46:21 +0500

    Update dataarray.jsx

--- src/components/dataarray.jsx
+++ src/components/dataarray.jsx
@@ -1,9 +1,22 @@
+import { useState } from 'react'
 import React from 'react'
 
 export const DataArray = () => {
+
+
+   const [obj , setobj ] = useState({
+    NAME: 'Sanwal '  , AGE : 23 , DEGREE : 'BSSE ' })
+
+    const changeobj = () => {
+    setobj( {...obj, NAME : 'Sanwal Khan' , DEGREE : 'BSCS'})
+       }
+
   return (
     <div>
-    I am Sanwal Khan
+   <h1>
+   Name : {obj.NAME} & Age : {obj.AGE} & Degree : {obj.DEGREE}
+   </h1>
+    <button onClick={ changeobj}>Update</button>
     </div>
   )
 }
